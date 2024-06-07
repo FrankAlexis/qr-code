89cc0298a02ec9d9c10dc8d6e2a28476f91e9980
Frank Alexis Castrillom Giraldo <franka@Franks-MacBook-Pro.local>
2024-06-07 15:49:13 -0500

    (feat): Add default color, and border color reference

--- src/app/ui/components/hexadecimalPicker/index.tsx
+++ src/app/ui/components/hexadecimalPicker/index.tsx
@@ -7,20 +7,19 @@ interface Props  {
     id: string
 }
 export default function HexadecimalPicker ({ label, id}: Props) {
-    const ref = useRef(null);
+    const ref = useRef<HTMLDivElement | null>(null);
     const [showPicker, setShowPicker] = useState(false)
     const [color, setColor] = useState<string>('#000000')
 
     useEffect(() => {
-        function handleClickOutside(_event: MouseEvent) {
-          if (ref.current) {
-            setShowPicker(prev => !prev)
-          }
+        function handleClickOutside(event: MouseEvent) {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+              setShowPicker(prev => !prev)
+            }
         }
-        // Bind the event listener
+        
         document.addEventListener("mousedown", handleClickOutside);
         return () => {
-          // Unbind the event listener on clean up
           document.removeEventListener("mousedown", handleClickOutside);
         };
       }, [ref]);
@@ -32,7 +31,15 @@ export default function HexadecimalPicker ({ label, id}: Props) {
                 className="block mb-2 text-sm font-medium text-black"
             >{label}</label>
             <input className='hidden' id={id} name={id} value={color} />
-            <button style={{ color: color }} onClick={() => setShowPicker(prev => !prev)} className="text-white end-2.5 bottom-2.5 border border-purple-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 h-max">{color}</button>
+            <button 
+              style={{ 
+                color: color, 
+                borderColor: color.startsWith('#f') ? 'gray' : color  
+              }} 
+              onClick={() => setShowPicker(prev => !prev)} 
+              className="text-white w-full end-2.5 bottom-2.5 border focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm p-2.5 h-max">
+              {color}
+            </button>
             {showPicker && 
                 <div ref={ref} className='relative'>
                     <SketchPicker className='absolute' color={color} onChange={(colorResult) => setColor(colorResult.hex)} />
